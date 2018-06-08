e3b1d0490b2e0558f6741a5800afa818e33c7a13
Jamie Yamashita <jamieyamashita@Jamies-MacBook-Pro.local>
2018-06-07 21:49:47 -1000

    Are Equal

--- functions.js
+++ functions.js
@@ -147,7 +147,13 @@ function isLessThan(a,b) {
  * @param {number} b
  * @return {boolean} the numbers are equal
  */
-
+function areEqual(a,b) {
+    if (a=b) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 /**
  * Returns the smallest value of two numbers.
