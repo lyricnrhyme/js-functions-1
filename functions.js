aa8c86a315fbe21e1f53084a3c420f332ca64f3a
Jamie Yamashita <jamieyamashita@Jamies-MacBook-Pro.local>
2018-06-07 21:14:04 -1000

    Found quotient of two numbers

--- functions.js
+++ functions.js
@@ -68,7 +68,10 @@ function multiply(x,y) {
  * @param {number} y
  * @return {number} the quotient
  */
-
+function divide(x,y) {
+    var quotient = x / y;
+    return quotient;
+}
 
 /**
  * Multiplies a number by itself.
